c4af5979614bbb1c8fe9761f8ef0c34d62a05676
wallacewylie <wallace.wylie@gmail.com>
2016-06-30 20:21:55 -0500

    clean up

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function validatePIN (pin) {
     var x = Math.floor(pin);
-    if (isNaN(x) === false && pin.length === 4 && pin.charAt(0) !== "." && pin.charAt(0) !== "-" && pin.charAt(0) !== "+" && pin.charAt(1) !== "e" && pin.charAt(1) !== "E") {
-        console.log(x.length);
-        return true;
-    } else if (isNaN(x) === false && pin.length === 6 && pin.charAt(0) !== "." && pin.charAt(0) !== "-" && pin.charAt(1) !== "e" && pin.charAt(0) !== "+" && pin.charAt(1) !== "E") {
+    if (isNaN(x) === false && pin.length === 4 && isNaN(x) === false && pin.length === 6 && pin.charAt(0) !== "." && pin.charAt(0) !== "-" && pin.charAt(0) !== "+" && pin.charAt(1) !== "e" && pin.charAt(1) !== "E") {
         return true;
     } else {
         return false;

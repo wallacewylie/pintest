0d1f7b6c626180d03f575f8e773210d5b35cf6f1
wallacewylie <wallace.wylie@gmail.com>
2016-06-30 20:23:30 -0500

    clean

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function validatePIN (pin) {
     var x = Math.floor(pin);
     if (isNaN(x) === false && pin.length === 4 && isNaN(x) === false && pin.length === 6 && pin.charAt(0) !== "." && pin.charAt(0) !== "-" && pin.charAt(0) !== "+" && pin.charAt(1) !== "e" && pin.charAt(1) !== "E") {
+        console.log(x.length);
+        return true;
+    } else if (isNaN(x) === false && pin.length === 6 && pin.charAt(0) !== "." && pin.charAt(0) !== "-" && pin.charAt(1) !== "e" && pin.charAt(0) !== "+" && pin.charAt(1) !== "E") {
         return true;
     } else {
         return false;
